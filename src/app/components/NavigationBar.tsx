86c95d9e11bc0670501abb68aa6dfbfceb0336f6
Daniel Nistreanu <142089952+dnistreanu1@users.noreply.github.com>
2025-04-18 18:12:46 +0300

    [ac-8] 1. Add Navigation Bar 2. Add Footer 3. Add Breadcrumb navigation (#3)
    Co-authored-by: Daniel Nistreanu <danielnistreanu@Daniels-MacBook-Pro.local>

---
+++ src/app/components/NavigationBar.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/app/components/generic/NavigationBarTemplate';
+import Image from 'next/image';
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: 'De vanzare',
+    href: '/buy',
+    description: '',
+  },
+  {
+    title: 'De inchiriat',
+    href: '/rent',
+    description: '',
+  },
+  {
+    title: 'Ansambluri rezidentiale',
+    href: '/residential',
+    description: '',
+  },
+];
+
+export function NavigationBar({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="flex h-16 items-center justify-center border-b-1">
+        <div className="flex w-3/4">
+          <NavigationMenu className="flex-1">
+            <NavigationMenuItem className="flex items-center mr-20">
+              <Link href={'/'} passHref>
+                <Image src="/content/images/logo.svg" alt="logo" width={204} height={52} className="pb-2" />
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuList>
+              {components.map((component, id) => (
+                <NavigationMenuItem key={id}>
+                  <Link href={component.href} passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>{component.title}</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+      {children}
+    </>
+  );
+}
+
+const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+              className
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
+ListItem.displayName = 'ListItem';
---
+++ src/app/components/generic/Icons.ts
@@ -0,0 +1,17 @@
+// src/components/icons/index.ts
+import {
+  Activity,
+  AlertCircle,
+  Check,
+  ChevronDown,
+  // add any additional icons you need
+} from 'lucide-react';
+
+// You can export them grouped inside an Icons object
+export const Icons = {
+  Activity,
+  AlertCircle,
+  Check,
+  ChevronDown,
+  // ... export additional icons as needed
+};
